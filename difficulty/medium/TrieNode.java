7d345d775fc6ed1acd5c0439e95b1e55182a3c36
zhangpeiyu <zhangpeiyu4@jd.com>
2019-11-24 18:12:26 +0800

    股票问题

---
+++ difficulty/medium/TrieNode.java
@@ -0,0 +1,16 @@
+package difficulty.medium;
+
+public class TrieNode {
+    //R links to children node
+    private TrieNode[] links;
+
+    private final int R = 26;
+
+
+    public TrieNode(){
+        links=new TrieNode[R];
+    }
+
+
+
+}
